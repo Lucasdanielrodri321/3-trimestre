d971d196b5230112a989d73c9cd03a08ab30ea52
Lucasdanielrodri321 <106817728+Lucasdanielrodri321@users.noreply.github.com>
2024-10-11 09:43:31 -0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,12 @@
+const getCSS = (variavel) => {
+    const bodyStyle = getComputedStyle(documemt.body)
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+  }
+  
+  const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+  }
+  
+  export { getCSS, tickConfig }
